8e368f0c222b7af9758c147a4c24b058b4b74a9f
Pancho Cholakov <panchor@abv.bg>
2022-08-13 13:09:00 +0300

    Add project files.

---
+++ Meetin 5/Meetin 5.cpp
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+
+{
+	/*Zadachi s chisla*/
+
+
+	/*int a = 43112;
+	int b = -13557674;
+	int c = 13557674;
+	int d = -1357674000;
+	long long int e = 3657895000;
+
+	printf(" a: %d\n b: %d\n c: %d\n d: %d\n e %lld\n", a,b,c,d,e);*/
+
+	/*```````````````````````````````````````````````````````````````*/
+	
+	/*long double a = 4.9876543;
+	long double b = 7.123456789012345678990;
+	long double c = 18398458438583853.28;
+	long double d = 18398458438583853.39875937284928422;
+
+	printf(" a: %.10Lf\n b: %.20Lf\n c: %.2Lf\n d: %.17Lf\n",(long double) a, b, (long double) c, (long double) d);*/
+
+
+	/*```````````````````````````````````````````````````````````````*/
+
+	/*enum Months { January=1, February=2, March=3, April=4, May=5, June=6, July=7, August=8, September=9, October=10, November=11, December=12 };
+
+	printf("The months of year are: %d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t", 1,2,3,4,5,6,7,8,9,10,11,12);*/
+
+	/*```````````````````````````````````````````````````````````````*/
+
+	/*enum MyFriends { Ivan=1, Stoyan=2, Petar=3, Milen=4, Ognyan=5 };
+	enum MyFriends Fr1, Fr2, Fr3, Fr4, Fr5;
+
+	
+	Fr1 = Ivan;
+	Fr2 = Stoyan;
+	Fr3 = Petar;
+	Fr4 = Milen;
+	Fr5 = Ognyan;	
+
+	printf("The number of my friends are:%d\t%d\t%d\t%d\t%d\t", Fr1,Fr2,Fr3,Fr4,Fr5);*/
+
+	/*```````````````````````````````````````````````````````````````*/
+
+	/*float TomatoWeight;
+	float TomatoPrice = 4.50;	
+	float CucumberWeight;
+	float CucumberPrice = 2.80;
+	float AppleWeight;
+	float ApplePrice = 2.50;
+	float CherryWeight;
+	float CherryPrice = 5.60;
+	float CucumberTotal;
+
+
+
+	printf("Hello, welcome to our vegetable store! Today we offer to you fresh tomatoes, cucumbers, apples and cherries!\n");
+
+	printf("\nHow much tomatoes would you like?: \n ");
+	scanf_s("%f", &TomatoWeight);
+	float TomatoTotal = TomatoWeight * TomatoPrice;
+	printf("\nWonderfull, your tomatoes price are: %.2f\n", TomatoTotal);
+
+	printf("\nHow much cucumbers would you like?: \n ");
+	scanf_s("%f", &CucumberWeight);
+	CucumberTotal = CucumberWeight * CucumberPrice;
+	printf("\nWonderfull, your cucumbers price are: %.2f\n", CucumberTotal);
+
+	printf("\nHow much apples would you like?: \n ");
+	scanf_s("%f", &AppleWeight);
+	float AppleTotal = AppleWeight * ApplePrice;
+	printf("\nWonderfull, your apples price are: %.2f\n", AppleTotal);
+
+	printf("\nHow much cherries would you like?: \n ");
+	scanf_s("%f", &CherryWeight);
+	float CherryTotal = CherryWeight * CherryPrice;
+	printf("\nWonderfull, your cherries price are: %.2f\n", CherryTotal);
+
+	float SumPrice = (TomatoTotal + CucumberTotal + AppleTotal + CherryTotal);
+	printf("\nYour total price for all purchases are: %.2f\n", SumPrice);*/
+
+	/*```````````````````````````````````````````````````````````````*/
+
+	/*char Who[25] = "I";
+	char Feeling[25] = "Like";
+	char What[25] = "to play football!";
+
+	printf("%s %s %s\n ", Who, Feeling, What);*/
+
+	/*```````````````````````````````````````````````````````````````*/
+
+
+
+
+
+
+	return 0;
+
+
+}
